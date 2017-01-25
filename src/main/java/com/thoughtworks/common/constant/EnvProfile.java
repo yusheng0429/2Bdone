e3d19145c9216034c29e4828cc0fa59f3a2b0e3d
syu <syu@thoughtworks.com>
2017-01-25 17:53:30 +0800

    Add a class EnvProfile in package com.thoughtworks.common.constant

---
+++ src/main/java/com/thoughtworks/common/constant/EnvProfile.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.common.constant;
+
+public class EnvProfile {
+    public static final String TEST = "test";
+    public static final String NOT_PRODUCTION = "!production";
+    public static final String PRODUCTION = "production";
+}
