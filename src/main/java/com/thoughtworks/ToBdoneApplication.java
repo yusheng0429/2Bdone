df41f9d24217fd6f230ee2da563985867984cd6d
syu <syu@thoughtworks.com>
2017-01-25 16:27:17 +0800

    Add class ToBdoneApplication with main function in the package com.thoughtworks

---
+++ src/main/java/com/thoughtworks/ToBdoneApplication.java
@@ -0,0 +1,11 @@
+package com.thoughtworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToBdoneApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ToBdoneApplication.class, args);
+    }
+}
