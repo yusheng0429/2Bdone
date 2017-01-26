4880108a1f8d55196cbd1b8378213df1bd59b1ac
syu <syu@thoughtworks.com>
2017-01-26 11:54:41 +0800

    Create a function initializeTasks with annotation @Before in class test.functional.task.TaskServiceFunctionalTest

--- src/test/java/test/functional/task/TaskServiceFunctionTest.java
+++ src/test/java/test/functional/task/TaskServiceFunctionTest.java
@@ -2,12 +2,23 @@ package test.functional.task;
 
 import com.thoughtworks.ToBdoneApplication;
 import com.thoughtworks.common.constant.EnvProfile;
+import com.thoughtworks.common.jpa.TaskRepository;
+import com.thoughtworks.entity.Task;
+import com.thoughtworks.service.TaskService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Rollback
@@ -15,4 +26,40 @@ import org.springframework.transaction.annotation.Transactional;
 @ActiveProfiles({EnvProfile.TEST})
 @SpringBootTest(classes = ToBdoneApplication.class)
 public class TaskServiceFunctionTest {
+    @Autowired
+    private TaskService taskSevice;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private List<Task> defaultTasks;
+
+    @Before
+    private void initializeTasks() {
+        Task task = new Task();
+        task.setName("Have breakfast");
+        task.setIsCompleted(true);
+        saveDefaultTask(task);
+
+        task = new Task();
+        task.setName("Read a book");
+        task.setIsCompleted(false);
+        saveDefaultTask(task);
+
+        task = new Task();
+        task.setName("Go to work");
+        task.setIsCompleted(true);
+        saveDefaultTask(task);
+
+        task = new Task();
+        task.setName("Go to the grocery store");
+        task.setIsCompleted(false);
+        saveDefaultTask(task);
+    }
+
+    private void saveDefaultTask(Task task) {
+        taskRepository.save(task);
+        defaultTasks.add(task);
+    }
 }
