155baad14c2a76fd9247e17aa6c7e1aa45033e83
syu <syu@thoughtworks.com>
2017-02-06 13:55:37 +0800

    Create a class TaskModel in package com.thoughtworks.model

---
+++ src/main/java/com/thoughtworks/model/TaskModel.java
@@ -0,0 +1,36 @@
+package com.thoughtworks.model;
+
+import java.util.Date;
+
+public class TaskModel {
+    private Long id;
+    private String name;
+    private boolean isCompleted;
+    private Date timeCreated;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getStatus() {
+        return this.isCompleted;
+    }
+
+    public void setStatus(boolean isCompleted) { this.isCompleted = isCompleted; }
+
+    public Date getTimeCreated() {
+        return this.timeCreated;
+    }
+
+    public void setTimeCreated(Date timeCreated) { this.timeCreated = timeCreated; }
+}
